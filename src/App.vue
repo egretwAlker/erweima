fd9517f726ba51bcf5359e8ad6ae5b8d39ba45d3
Tianxiang Xia <egrr353@protonmail.com>
2022-12-27 11:08:04 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import './index.css';
+import QrcodeVue from 'qrcode.vue';
+import {QrStream, QrCapture, QrDropzone} from 'vue3-qr-reader';
+
+export default {
+  data() {
+    return {
+      message: "https://google.com",
+      fc: 0
+    }
+  },
+  methods: {
+    onDecode(s) {
+      this.message = s;
+    }
+  },
+  components: {
+    QrcodeVue,
+    QrStream,
+    QrCapture,
+    QrDropzone
+  }
+};
+</script>
+
+<template>
+<div class="flex-row items-center mx-auto max-w-md min-h-screen bg-gray-900 shadow">
+<input class="rounded-lg bg-gray-500 w-full" type="text" v-model="message" />
+<div @click="fc = 1-fc">
+<QrcodeVue v-if="fc === 0" :value="message" class="mx-auto"></QrcodeVue>
+<QrStream v-else @decode="onDecode" class="mx-auto"/>
+</div>
+</div>
+</template>
